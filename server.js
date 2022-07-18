389fb61ad745bba85fdf932cd2c9ebc88b728e2c
Cheryl M <webmaster@cherylli.com>
2022-07-18 14:16:02 +0000

    commit for import to codesandbox

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const LocalStrategy = require('passport-local')
+const ObjectID = require('mongodb').ObjectID
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -10,14 +14,52 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env['SESSION_SECRET'],
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-  res.render('index');
+
+myDB(async client => {
+  const myDataBase = await client.db('qaAdvancedNode').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  // Serialization and deserialization here...
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => {
+      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, doc);
+      });
+});
+    
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to login' });
+  });
 });
 
-const PORT = process.env.PORT || 3000;
+
+
+
+const PORT = process.env['PORT'] || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
