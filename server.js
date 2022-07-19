5a7bec3d8dc40fd73ab6d8e9d3504e9b48dbc063
Cheryl M <webmaster@cherylli.com>
2022-07-20 00:40:05 +1000

    add passport local strategy

--- server.js
+++ server.js
@@ -1,65 +1,88 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const passport = require('passport');
-const session = require('express-session');
-const LocalStrategy = require('passport-local')
-const ObjectID = require('mongodb').ObjectID
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local");
+const ObjectID = require("mongodb").ObjectID;
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: process.env['SESSION_SECRET'],
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env["SESSION_SECRET"],
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
 
-
-myDB(async client => {
-  const myDataBase = await client.db('qaAdvancedNode').collection('users');
+myDB(async (client) => {
+  const myDataBase = await client.db("qaAdvancedNode").collection("users");
 
   // Be sure to change the title
-  app.route('/').get((req, res) => {
+  app.route("/").get((req, res) => {
     //Change the response to render the Pug template
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please login'
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
+      showLogin: true
     });
   });
 
+  app.post(
+    "/login",
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+  );
   // Serialization and deserialization here...
-    passport.serializeUser((user, done) => {
-      done(null, user._id);
-    });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
 
-    passport.deserializeUser((id, done) => {
-      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(null, doc);
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log("User " + username + " attempted to log in.");
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
+        return done(null, user);
       });
-});
-    
+    })
+  );
+
   // Be sure to add this...
-}).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to login' });
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("index", { title: e, message: "Unable to login" });
   });
 });
 
-
-
-
-const PORT = process.env['PORT'] || 3000;
+const PORT = process.env["PORT"] || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
